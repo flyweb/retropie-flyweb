e7d95d3edd446e450d21fed175c114652200442d
irenewchen <irene.chen@alumni.ubc.ca>
2017-01-21 14:12:01 -0700

    cleanup deletion

--- routes/index.js
+++ routes/index.js
@@ -17,24 +17,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/files', function (req, res, next) {
-  var files = listFiles(path);
-
-  res.render('files', {
-    layout: 'layouts/admin',
-    title: 'List of Games',
-    files: files
-  });
+  files = listFiles(path);
+  returnFiles(files, res);
 });
 
-router.post('/upload', function (req, res, next) {
-  var args = [];
+router.post('/files/upload', function (req, res, next) {
   var file = req.files[0];
-  var filePath = args.concat(file.path);
+  var filePath = file.path;
   var originalName = file.originalname;
   var encoding = file.encoding;
   var newFileName = path + "\\" + originalName;
 
-  var tempFile = fs.readFileSync(filePath[0]);
+  var tempFile = fs.readFileSync(filePath);
   fs.writeFile(newFileName, tempFile, function (err) {
     if (err) {
       return console.log(err);
@@ -42,18 +36,12 @@ router.post('/upload', function (req, res, next) {
     console.log("Sucessfully uploaded to " + newFileName);
   });
 
-  // show the list of files again, including the file that was just uploaded
-  // shows the file that was just uploaded first (at the top of the list)
   var files = listFiles(path);
   files.unshift(originalName);
-  res.render('files', {
-    layout: 'layouts/admin',
-    title: 'List of Games',
-    files: files
-  });
+  returnFiles(files, res);
 });
 
-router.post('/delete/:name', function (req, res, next) {
+router.post('/files/delete/:name', function (req, res, next) {
   var name = req.params.name;
   console.log("received delete for " + name);
 
@@ -64,23 +52,30 @@ router.post('/delete/:name', function (req, res, next) {
     }
     console.log(name + " deleted successfully");
   });
+  
+  files = removeDeletedFileFromList(files, name);
+  returnFiles(files, res);
+});
 
+function listFiles(path) {
+  return fs.readdirSync(path);
+}
+
+function removeDeletedFileFromList(files, name) {
   var files = listFiles(path);
   var index = files.indexOf(name);
-
   if (index > -1) {
     files.splice(index, 1);
   }
-  
+  return files;
+}
+
+function returnFiles(files, res) {
   res.render('files', {
     layout: 'layouts/admin',
     title: 'List of Games',
     files: files
   });
-});
-
-function listFiles(path) {
-  return fs.readdirSync(path);
 }
 
 module.exports = router;
